0558a8f24c4e90e8d5e1020d6c4fd0e64586870d
Sarah Roberts <sonoran.sarah@gmail.com>
2026-08-14 17:42:15 -0700

    Version 2.1.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/metadata-files "2.1.2"
+(defproject org.cyverse/metadata-files "2.1.3-SNAPSHOT"
   :description "Library for generating metadata files."
   :url "https://github.com/cyverse-de/metadata-files"
   :license {:name "BSD"
